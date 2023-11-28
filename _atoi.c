2f018776d95eefd8d16823ea7a933a22cd079a8b
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-11-28 09:43:52 -0600

    Update _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,1 +1,78 @@
+Certainly! Let me break down the `atoi` function into individual code blocks along with explanations:
 
+### Code Block 1: Function Signature and Initialization
+
+```c
+int atoi(const char *str) {
+    int result = 0;
+    int sign = 1;  // Default sign is positive
+```
+
+Explanation:
+- The function `atoi` takes a pointer to a constant character (`const char *`) as an argument, representing the input string.
+- It initializes two variables: `result` to store the final converted integer and `sign` to represent the sign of the number (default is positive).
+
+### Code Block 2: Skip Leading Whitespaces
+
+```c
+    // Skip leading white spaces
+    while (*str == ' ' || (*str >= '\t' && *str <= '\r')) {
+        str++;
+    }
+```
+
+Explanation:
+- This loop skips any leading whitespaces in the input string. It uses the condition `(*str >= '\t' && *str <= '\r')` to check for various whitespace characters, including tab and newline.
+
+### Code Block 3: Handle the Sign
+
+```c
+    // Handle the sign
+    if (*str == '-' || *str == '+') {
+        sign = (*str++ == '-') ? -1 : 1;
+    }
+```
+
+Explanation:
+- Checks if the current character is a minus or plus sign.
+- If a minus sign is found, it updates the `sign` variable to -1; otherwise, it remains 1.
+- The pointer `str` is incremented if a sign is present.
+
+### Code Block 4: Convert the String to an Integer
+
+```c
+    // Convert the string to an integer
+    while (*str >= '0' && *str <= '9') {
+        result = result * 10 + (*str++ - '0');
+    }
+```
+
+Explanation:
+- This loop iterates through the string's numeric characters ('0' to '9') and converts them to an integer.
+- It multiplies the current result by 10 and adds the numeric value of the current character.
+
+### Code Block 5: Return the Result
+
+```c
+    return sign * result;
+}
+```
+
+Explanation:
+- The function returns the final result, considering the sign of the number.
+
+### Code Block 6: Example Usage in `main` Function
+
+```c
+int main() {
+    const char *str = "12345";
+    int num = atoi(str);
+    printf("Converted number: %d\n", num);
+
+    return 0;
+}
+```
+
+Explanation:
+- This is an example `main` function that demonstrates the usage of the `atoi` function with a string "12345".
+- It prints the converted number to the console.

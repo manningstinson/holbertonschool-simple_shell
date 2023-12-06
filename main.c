eb9768116a2cc222813e2d7fd16e1fa81cade1a9
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-06 10:01:20 -0600

    Update main.c

--- main.c
+++ main.c
@@ -12,38 +12,38 @@
 int entry_point(void)
 {
     /* Initialize shell information */
-    info_t shell_info[] = { INFO_INIT };
+    info_t shell_info = INFO_INIT;
 
     /* Check if a file is provided as an argument */
     int file_descriptor = 2;
 
-    if (shell_info->argument_count == 2)
+    if (shell_info.argument_count == 2)
     {
-        file_descriptor = open(shell_info->argument_vector[1], O_RDONLY);
+        file_descriptor = open(shell_info.argument_vector[1], O_RDONLY);
         if (file_descriptor == -1)
         {
             if (errno == EACCES)
                 exit(126);
             if (errno == ENOENT)
             {
-                _eputs(shell_info->argument_vector[0]);
+                _eputs(shell_info.argument_vector[0]);
                 _eputs(": 0: Unable to open ");
-                _eputs(shell_info->argument_vector[1]);
+                _eputs(shell_info.argument_vector[1]);
                 _eputchar('\n');
                 _eputchar(BUF_FLUSH);
                 exit(127);
             }
             return EXIT_FAILURE;
         }
-        shell_info->readfd = file_descriptor;
+        shell_info.readfd = file_descriptor;
     }
 
     /* Initialize environment and read command history */
-    populate_env_list(shell_info);
-    read_history(); /* Corrected function call */
+    populate_env_list(&shell_info);
+    read_history();
 
     /* Launch the shell */
-    shell_loop(shell_info);
+    shell_loop(&shell_info);
 
     return EXIT_SUCCESS;
 }
@@ -57,10 +57,3 @@ int main(void)
 {
     return entry_point();
 }
-
-/* Correct definition of read_history() */
-void read_history(void)
-{
-    /* Your implementation for reading history */
-}
-

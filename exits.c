cc732708cd949ddeb7139215cb0e3d48e18d2044
Manning Stinson <manning@manningstinson.com>
2023-12-06 14:13:11 -0800

    updated betty styles

--- exits.c
+++ exits.c
@@ -9,25 +9,26 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i, j;
-    char *s = dest;
+	int i, j;
+	char *s = dest;
 
-    i = 0;
-    while (src[i] != '\0' && i < n - 1)
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    if (i < n)
-    {
-        j = i;
-        while (j < n)
-        {
-            dest[j] = '\0';
-            j++;
-        }
-    }
-    return (s);
+	i = 0;
+
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
 }
 
 /**
@@ -37,41 +38,41 @@ char *_strncpy(char *dest, char *src, int n)
  * @n: the amount of bytes to be maximally used
  * Return: the concatenated string
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-    int i, j;
-    char *s = dest;
+	int i, j;
+	char *s = dest;
 
-    i = 0;
-    j = 0;
-    while (dest[i] != '\0')
-        i++;
-    while (src[j] != '\0' && j < n)
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
-    if (j < n)
-        dest[i] = '\0';
-    return (s);
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
 }
 
 /**
  ** _strchr - locates a character in a string
  * @s: the string to be parsed
  * @c: the character to look for
- * Return: a pointer to the first occurrence of the character c in the string s,
- *         or NULL if the character is not found
+ * Return: a pointer to the first occurrence of the character c
  */
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
-        if (*s == c)
-            return s;
-        s++;
-    }
-    return NULL;
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
 }
-
